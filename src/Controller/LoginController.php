d241b1a8c53050164d01ef3cdc308976c979f97c
Kris <62119861+kris86000@users.noreply.github.com>
2022-09-28 17:18:39 +0200

    revoir session

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
 
 class LoginController extends AbstractController
 {
@@ -14,10 +17,10 @@ class LoginController extends AbstractController
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-    
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-       
+
 
         return $this->render('login/index.html.twig', [
             // 'controller_name' => 'LoginController',
--- src/Controller/MainPageController.php
+++ src/Controller/MainPageController.php
@@ -4,19 +4,43 @@ namespace App\Controller;
 
 use App\Entity\Articles;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\PanierService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Entity\Orders;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class MainPageController extends AbstractController
 {
 
     #[Route('/', name: 'app_main_page')]
 
-    public function affichageArticle(ManagerRegistry $doctrine): Response
+    public function affichageArticle(ManagerRegistry $doctrine, AuthenticationUtils $authenticationUtils): Response
     {
+        $session = new Session();
+        $session->start();
         $allArticles = $doctrine->getRepository(Articles::class)->findAll();
-
+        $amount = 0;
+        $quantity = 0;
+        $lastUsername = $authenticationUtils->getLastUsername();
+        if ($lastUsername != null) {
+            $user = $doctrine->getRepository(User::class)->findOneBy(['email' => $lastUsername]);
+            $userId = $user->getId();
+            $order = $doctrine->getRepository(Orders::class)->findOneBy(['user' => $userId, 'status' => 'panier']);
+            if ($order != null) {
+                $amount = $order->getAmount();
+                foreach ($order->getOrdersLines() as $orderline) {
+                    $quantity = $quantity + $orderline->getQuantity();
+                }
+            }
+        }
+        $request = new RequestStack;
+        $servicePanier = new PanierService($request);
+        $servicePanier->majPanier($amount, $quantity);
         return $this->render('main_page/index.html.twig', [
             'controller_name' => 'MainPageController',
             'allArticles' => $allArticles
